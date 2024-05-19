652e28c436334f00de1c13ecea8f08915a597030
poyiray <85799454+poyiray@users.noreply.github.com>
2024-05-19 12:46:33 -0400

    ccc 97 s3

---
+++ Project1/CCC '97 S3 - Double Knockout Competition.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int m, n;
+
+int main()
+{
+	//freopen("out.txt", "w", stdout);
+	cin >> m;
+	while (m--)
+	{
+		int ud, ol = 0, el = 0, cnt = 0;
+		cin >> ud;
+		while (ud != 0 || ol != 1)
+		{
+			cout << "Round " << cnt++ << ": " << ud << " undefeated, " << ol << " one-loss, " << el << " eliminated" << endl;
+			if (ud == 1 && ol == 1)
+			{
+				ud = 0;
+				ol = 2;
+			}
+			else
+			{
+				el += ol / 2;
+				ol = ol - ol / 2 + ud / 2;
+				ud -= ud / 2;
+			}
+		}
+		cout << "Round " << cnt++ << ": " << ud << " undefeated, " << ol << " one-loss, " << el << " eliminated" << endl;
+		cout << "There are " << cnt - 1 << " rounds." << endl << endl;
+	}
+
+	return 0;
+}
