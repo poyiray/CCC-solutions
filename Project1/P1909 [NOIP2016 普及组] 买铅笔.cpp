f962f5c8edbb2623ebf56c9e7756012143b5e0f7
poyiray <85799454+poyiray@users.noreply.github.com>
2024-07-14 14:56:46 -0400

    1

---
+++ Project1/P1909 [NOIP2016 普及组] 买铅笔.cpp
@@ -0,0 +1,17 @@
+﻿// #pragma GCC optimize(2)
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, v1, v2, v3, c1, c2, c3;
+
+int main()
+{
+	cin >> n >> v1 >> c1 >> v2 >> c2 >> v3 >> c3;
+
+	int a = ceil(n * 1.0 / v1) * c1;
+	int b = ceil(n * 1.0 / v2) * c2;
+	int c = ceil(n * 1.0 / v3) * c3;
+	cout << min({ a,b,c });
+
+	return 0;
+}
