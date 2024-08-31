70b75345f6ae225a3020879f7aa7ccab6f1f232c
poyiray <85799454+poyiray@users.noreply.github.com>
2024-08-31 17:24:49 -0400

    ccc 12 s4 coin game

--- Project1/CCC '12 S4 - A Coin Game.cpp
+++ Project1/CCC '12 S4 - A Coin Game.cpp
@@ -2,66 +2,103 @@
 using namespace std;
 
 const int N = 10;
-int n, res = INT_MAX;
-vector<int> s[N];
 
-void dfs(int x, int u, int v)
+struct node
 {
-	for (int i = 1; i <= n; i++)
-	{
-		for (auto v : s[i])
-		{
-			cout << v << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-	for (int i = 1; i <= n; i++)
-	{
-		if (!s[i].size()) continue;
-		if(i != v || i + 1 != u)
-		if (!s[i + 1].size() || s[i].back() < s[i + 1].back())
-		{
-			s[i + 1].push_back(s[i].back());
-			s[i].pop_back();
-			dfs(x + 1, i, i + 1);
-			s[i].push_back(s[i + 1].back());
-			s[i + 1].pop_back();
-		}
+	vector<vector<int>> a;
+	int x;
+};
 
-		else if(i != v || i - 1 != u)
-		if (!s[i - 1].size() || s[i].back() < s[i - 1].back())
-		{
-			s[i - 1].push_back(s[i].back());
-			s[i].pop_back();
-			dfs(x + 1, i, i - 1);
-			s[i].push_back(s[i - 1].back());
-			s[i - 1].pop_back();
-		}
-	}
+int n;
+set<string> vis;
 
+bool judge(vector<vector<int>>& a)
+{
+	if (a[1].empty()) return 0;
 	for (int i = 2; i <= n; i++)
 	{
-		if (s[i - 1].size() > 1 || s[i].size() > 1) return;
-		if (s[i - 1].back() > s[i].back()) return;
+		if (a[i].empty()) return 0;
+		if (a[i].back() < a[i - 1].back()) return 0;
 	}
-	res = min(res, x);
+	return 1;
+}
+
+string get_id(vector<vector<int>>& a)
+{
+	string id = " ";
+	for (int i = 1; i <= n; i++)
+	{
+		if (a[i].empty()) id += ' ';
+		else for (auto& c : a[i]) id += c + '0';
+		id += '-';
+	}
+	return id;
+}
+
+int bfs(vector<vector<int>> &v)
+{
+	string s;
+	queue<node> q;
+	q.push({ v, 0 });
+	vis.insert(get_id(v));
+
+	while (q.size())
+	{
+		vector<vector<int>> a = q.front().a;
+		int x = q.front().x;
+		q.pop();
+		if (judge(a)) return x;
+
+		for (int i = 1; i <= n; i++)
+		{
+			if (a[i].empty()) continue;
+			if (a[i - 1].empty() || a[i - 1].back() > a[i].back())
+			{
+				a[i - 1].push_back(a[i].back());
+				a[i].pop_back();
+				s = get_id(a);
+				if (!vis.count(s))
+				{
+					vis.insert(s);
+					q.push({ a, x + 1 });
+				}
+				a[i].push_back(a[i - 1].back());
+				a[i - 1].pop_back();
+			}
+
+			if (a[i + 1].empty() || a[i + 1].back() > a[i].back())
+			{
+				a[i + 1].push_back(a[i].back());
+				a[i].pop_back();
+				s = get_id(a);
+				if (!vis.count(s))
+				{
+					vis.insert(s);
+					q.push({ a, x + 1 });
+				}
+				a[i].push_back(a[i + 1].back());
+				a[i + 1].pop_back();
+			}
+		}
+	}
+	return -1;
 }
 
 int main()
 {
+	ios::sync_with_stdio(false); cin.tie(nullptr);
 	while (cin >> n && n)
 	{
-		s[0].push_back(INT_MIN);
-		s[n + 1].push_back(INT_MIN);
+		vector<vector<int>> v(N); string s = " ";
+		v[0].push_back(-1e6), v[n + 1].push_back(-1e6);
 		for (int i = 1; i <= n; i++)
 		{
 			int x; cin >> x;
-			s[i].push_back(x);
+			v[i].push_back(x);
 		}
-		cout << endl;
-		dfs(0, 0, 0);
-		cout << res << '\n';
+		int res = bfs(v); vis.clear();
+		if (!~res) cout << "IMPOSSIBLE\n";
+		else cout << res << '\n';
 	}
 
 	return 0;
