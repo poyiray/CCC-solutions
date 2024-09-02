ac8b20e83d1e98c9c94d0c43f39bda6046ef8316
poyiray <85799454+poyiray@users.noreply.github.com>
2024-09-02 18:47:54 -0400

    ccc

---
+++ Project1/CCC '11 S5 - Switch.cpp
@@ -0,0 +1,89 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+const int N = (1 << 25) + 1;
+int n, vis[N];
+
+void ck(int loc, int &x)
+{
+	int l, r;
+	l = r = loc;
+	for (int i = loc - 1; i >= 0; i--)
+	{
+		if (!(x >> i & 1)) break;
+		l--;
+	}
+	for (int i = loc + 1; i < n; i++)
+	{
+		if (!(x >> i & 1)) break;
+		r++;
+	}
+	if (r - l + 1 >= 4)
+	{
+		int mask = ((1 << r + 1) - 1) ^ (1 << l) - 1;
+		x ^= mask;
+	}
+}
+
+int main()
+{
+	cin >> n;
+	int num = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		int x; cin >> x;
+		num = (num << 1) | x;
+	}
+
+	queue<pair<int, int>> q;
+	q.push({ num, 0 });
+	while (q.size())
+	{
+		int cnt = q.front().second, x = q.front().first; q.pop();
+		if (!x)
+		{
+			cout << cnt;
+			return 0;
+		}
+		int id = x;
+		for (int i = 0; i < n; i++)
+		{
+			x = id;
+			if (i == 0)
+			{
+				if (x & 1 << (i + 1) && !(x >> i & 1))
+				{
+					x |= 1 << i;
+					ck(i, x);
+					if (vis[x]) continue;
+					vis[x] = 1;
+					q.push({ x, cnt + 1 });
+				}
+			}
+			else if (i == n - 1)
+			{
+				if (x & 1 << (i - 1) && !(x >> i & 1))
+				{
+					x |= 1 << i;
+					ck(i, x);
+					if (vis[x]) continue;
+					vis[x] = 1;
+					q.push({ x, cnt + 1 });
+				}
+			}
+			else
+			{
+				if ((x & 1 << (i - 1) || x & 1 << (i + 1)) && !(x >> i & 1))
+				{
+					x |= 1 << i;
+					ck(i, x);
+					if (vis[x]) continue;
+					vis[x] = 1;
+					q.push({ x, cnt + 1 });
+				}
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ Project1/CCC '97 S5 - Long Division.cpp
@@ -0,0 +1,64 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 100;
+int a[N], b[N], c[N];
+char A[N], B[N];
+
+bool cmp(int last)
+{
+	if (a[last + b[0]]) return 1;
+	for (int i = b[0]; i >= 1; i--)
+	{
+		if (a[last + i - 1] < b[i]) return 0;
+		if (a[last + i - 1] > b[i]) return 1;
+	}
+	return 1;
+}
+
+void div()
+{
+	for (int i = c[0]; i >= 1; i--)
+	{
+		while (cmp(i))
+		{
+			for (int j = 1; j <= b[0]; j++)
+			{
+				a[i + j - 1] -= b[j];
+				if (a[i + j - 1] < 0)
+				{
+					a[i + j] -= 1;
+					a[i + j - 1] += 10;
+				}
+			}
+			c[i]++;
+		}
+	}
+	if (c[0] >= 2 && !c[c[0]]) c[0]--;
+	while (a[0] >= 2 && !a[a[0]]) a[0]--;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	int _; cin >> _;
+	while (_--)
+	{
+		memset(c, 0, sizeof c);
+		memset(a, 0, sizeof a);
+		memset(b, 0, sizeof b);
+		cin >> A + 1 >> B + 1;
+		a[0] = strlen(A + 1);
+		b[0] = strlen(B + 1);
+		c[0] = a[0] - b[0] + 1;
+		for (int i = a[0]; i >= 1; i--) a[a[0] - i + 1] = A[i] - '0';
+		for (int i = b[0]; i >= 1; i--) b[b[0] - i + 1] = B[i] - '0';
+		div();
+		if (c[0] <= 0) cout << 0;
+		else for (int i = c[0]; i >= 1; i--) cout << c[i]; cout << '\n';
+		for (int i = a[0]; i >= 1; i--) cout << a[i]; cout << '\n';
+		if (_) cout << '\n';
+	}
+
+	return 0;
+}
