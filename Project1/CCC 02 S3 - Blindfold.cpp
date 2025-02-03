62e7587146592d6b48b615b1e27a1a049b750d04
poyiray <85799454+poyiray@users.noreply.github.com>
2025-02-03 13:12:00 -0500

    123

--- Project1/CCC 02 S3 - Blindfold.cpp
+++ Project1/CCC 02 S3 - Blindfold.cpp
@@ -1,28 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 30010;
-int d[4][2] ={{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-char g[400][90];
-vector<pair<int, int>> loc[4];
-char mov[N];
-int r, c, m;
-void init()
+const int N = 400, M = 90;
+struct node
 {
-	loc[0].push_back({ 0,0 }); loc[1].push_back({ 0,0 });
-	loc[2].push_back({ 0,0 }); loc[3].push_back({ 0,0 });
-	for (int i = 0; i < 4; i++)
+	int x, y;
+	bool operator<(const node& n) const
 	{
-		int fa = i;
-		for (int j = 0; j < m; j++)
+		if (x == n.x) return y < n.y;
+		return x < n.x;
+	}
+};
+int r, c, m, vx, vy, wl, wr, wu, wd;
+set<node> pos;
+char a[N][M], q[30010];
+
+int dx[] = { -1, 0, 1, 0 };
+int dy[] = { 0, 1, 0,-1 };
+
+int mod(int a, int b) { return (a % b + b) % b; }
+
+void init(int d)
+{
+	pos.clear();
+	vx = vy = 0;
+	wl = wr = wu = wd = 0;
+	for (int i = 1; i <= m; i++)
+	{
+		if (q[i] == 'F')
 		{
-			if (mov[j] == 'R') fa = (fa + 1) % 4;
-			else if (mov[j] == 'L') fa = (fa - 1 == -1 ? 3 : fa - 1);
-			else if (mov[j] == 'F')
+			vx += dx[d];
+			vy += dy[d];
+			wl = min(wl, vy);
+			wr = max(wr, vy);
+			wu = min(wu, vx);
+			wd = max(wd, vx);
+			pos.insert({ vx,vy });
+		}
+		else
+		{
+			d += (q[i] == 'L' ? -1 : 1);
+			d = mod(d, 4);
+		}
+	}
+}
+
+void solve()
+{
+	for (int i = 1; i <= r; i++)
+	{
+		for (int j = 1; j <= c; j++)
+		{
+			if (a[i][j] == 'X' || a[vx + i][vy + j] == 'X') continue;
+			if (wl + j <= 0 || wr + j > c) continue;
+			if (wu + i <= 0 || wd + i > r) continue;
+			int f = false;
+			for(auto [x,y] : pos)
 			{
-				int x = loc[i].back().first, y = loc[i].back().second;
-				loc[i].push_back({ x + d[fa][0], y + d[fa][1] });
+				x += i, y += j;
+				if (x <= 0 || x > r || y <= 0 || y > c || a[x][y] == 'X') { f = true; break; }
 			}
+			if (!f) a[vx + i][vy + j] = '*';
 		}
 	}
 }
@@ -31,37 +69,21 @@ int main()
 {
 	ios::sync_with_stdio(false); cin.tie(nullptr);
 	cin >> r >> c;
-	for (int i = 0; i < r; i++) cin >> g[i];
+	for (int i = 1; i <= r; i++) cin >> a[i] + 1;
 	cin >> m;
-	for (int i = 0; i < m; i++) cin >> mov[i];
-	init();
-	for (int k = 0; k < 4; k++)
+	for (int i = 1; i <= m; i++) cin >> q[i];
+
+	for (int i = 0; i < 4; i++)
 	{
-		for (int i = 0; i < r; i++)
-		{
-			for (int j = 0; j < c; j++)
-			{
-				if (loc[k].back().first + i < 0 || loc[k].back().first + i > r) continue;
-				if (loc[k].back().second + j < 0 || loc[k].back().second + j > c) continue;
-				bool flag = false;
-				int x, y;
-				for (auto m : loc[k])
-				{
-					x = m.first + i, y = m.second + j;
-					if (x < 0 || x > r || y < 0 || y > c || g[x][y] == 'X') { flag = true; break; }
-				}
-				if (!flag) g[x][y] = '*';
-			}
-		}
+		init(i);
+		solve();
 	}
-	for (int i = 0; i < r; i++)
+	for (int i = 1; i <= r; i++)
 	{
-		for (int j = 0; j < c; j++)
-		{
-			cout << g[i][j];
-		}
+		for (int j = 1; j <= c; j++)
+			cout << a[i][j];
 		cout << '\n';
 	}
-
+	
 	return 0;
 }
